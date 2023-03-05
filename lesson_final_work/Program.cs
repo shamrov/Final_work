6e0cbf61ad270297292aab5573caf86ce7354470
Denis Shamrov <d_shamrov@mail.ru>
2023-03-05 14:32:29 +0300

    Added completed final work task

--- lesson_final_work/Program.cs
+++ lesson_final_work/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] ParseArray(string[] array, int elements)
+{
+    string[] result = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= elements)
+        {
+            result[count] += array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.WriteLine($"[{String.Join(", ",array)}]");
+}
+
+string[] array = { "hello", "2", "world", ":-)" };
+string[] arrayParse = ParseArray(array, 3);
+PrintArray(array);
+System.Console.WriteLine();
+PrintArray(arrayParse);
+
+
+// Вариант с простым выводом
+
+// void ParseArray(string[] array, int elements)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= elements)
+//         {
+//             System.Console.WriteLine(array[i]);
+//         }
+//     }
+// }
+
+
+// ParseArray(array, 3);
